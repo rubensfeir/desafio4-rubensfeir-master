851e1fb5d722b22ad0f665be8ffe7aea0579904e
Rubén Sfeir <ruben.sfeir@ing.uchile.cl>
2021-06-05 19:09:24 -0400

    Initial commit

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from mainApp.forms import Formulario
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from mainApp.models import Usuario
 # Create your views here.
 def index(request):
     return render(request, 'mainApp/index.html')
@@ -15,10 +16,16 @@ def formulario(request):
 def resultados(request):
     fname = request.POST['fname']
     lname = request.POST['lname']
+    username= request.POST['username']
+    password = request.POST['password']
+    user = User.objects.create_user(username=username, password=password)
+
+    usuario = Usuario(user=user, fname=fname, lname=lname)
+    usuario.save()
 
     context = {}
     context['fname'] = fname
     context['lname'] = lname
-
+    context['username'] = username
 
     return render(request, 'mainApp/resultados.html', context)
